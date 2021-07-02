a2db484f203347510a9cd749eec80adfef9283dc
Finances of the Nation <67075257+FONCanada@users.noreply.github.com>
2021-07-02 14:47:13 -0600

    Create setup.R

---
+++ R/setup.R
@@ -0,0 +1,61 @@
+# Sets up the main packages
+packages<-c("curl","scales","zoo","dplyr","tidyr","testit","data.table","googlesheets4")
+check.packages <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg,type="binary") # installing from source takes significantly longer on Github Actions
+  sapply(pkg, require, character.only = TRUE)
+}
+check.packages(packages)
+
+# List of Provinces
+provsort<-c("BC","AB","SK","MB","ON","QC","NB","NS","PE","NL")
+provinces<-c("Canada","Newfoundland and Labrador","Prince Edward Island","Nova Scotia",
+             "New Brunswick","Quebec","Ontario","Manitoba","Saskatchewan",
+             "Alberta","British Columbia","Yukon","Northwest Territories","Nunavut")
+provinces2<-c("CAN","NL","PE","NS",
+              "NB","QC","ON","MB","SK",
+              "AB","BC","YT","NT","NU")
+provnames<-data.frame(GEO=provinces,short=provinces2)
+provnames$short <- factor(provnames$short, levels = c("CAN","BC","AB","SK","MB","ON","QC","NB","NS","PE","NL","YT","NT","NU")) # Lock in factor level order
+
+# Code to fetch Statistics Canada files
+getTABLE<-function(x) {
+  url<-paste0("https://www150.statcan.gc.ca/n1/tbl/csv/",x,"-eng.zip")
+  temp<-tempfile()
+  download.file(url,temp)
+  if (has_warning(unzip(temp,paste0(x,".csv")))) { # Avoids html landing page
+    download.file(url,temp)
+  }
+  unzip(temp,paste0(x,".csv"))
+  rawdata<-fread(paste0(x,".csv"),encoding="UTF-8",stringsAsFactors=FALSE)
+  colnames(rawdata)[1]<-"Ref_Date"
+  data<-rawdata %>%
+    dplyr::rename(Value=VALUE) %>%
+    select(-UOM_ID,-SCALAR_ID) %>%
+    dplyr::rename_all(list(~make.names(.))) # this replaces the spaces with dots in the column names
+  if (class(data$Ref_Date)=="character" & !grepl("/",data[1,"Ref_Date"])){
+    data<-data %>%
+      mutate(Ref_Date=as.yearmon(Ref_Date))
+  }
+  if ("GEO" %in% colnames(data)){
+    data <- data %>%
+      left_join(provnames,by="GEO")
+  }
+  if ("North.American.Industry.Classification.System..NAICS." %in% colnames(data)){
+    data <- data %>%
+      rename(NAICS=North.American.Industry.Classification.System..NAICS.) %>%
+      mutate(NAICScode=str_match(NAICS,"\\[(.*?)\\]")[,2],
+             NAICS=ifelse(regexpr(" \\[",NAICS)>1,
+                          substr(NAICS,1,regexpr(" \\[",NAICS)-1),NAICS))
+  }
+  if (any(grepl("North.American.Product.Classification.System..NAPCS.",colnames(data)))){
+    colnames(data)[grepl("North.American.Product.Classification.System..NAPCS.",colnames(data))]<-"NAPCS"
+    data <- data %>%
+      mutate(NAPCS=ifelse(regexpr(" \\[",NAPCS)>1,
+                          substr(NAPCS,1,regexpr(" \\[",NAPCS)-1),NAPCS))
+  }
+  sourcetable<-gsub("(\\d{2})(\\d{2})(\\d{4})$","\\1-\\2-\\3",x)
+  comment(data)<-paste("Statistics Canada data table",sourcetable)
+  return(data)
+}
